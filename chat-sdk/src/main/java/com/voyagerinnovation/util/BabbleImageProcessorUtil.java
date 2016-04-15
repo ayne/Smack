8e51e5b947c1b0ddce40ecb20a18e1aeb694ba6f
Kim Arvin Silvoza <kim_vincible@yahoo.com>
2016-04-15 11:29:05 +0800

    Version 1.1.12. Added null checks in library

--- chat-sdk/src/main/java/com/voyagerinnovation/util/BabbleImageProcessorUtil.java
+++ chat-sdk/src/main/java/com/voyagerinnovation/util/BabbleImageProcessorUtil.java
@@ -8,6 +8,7 @@ import android.media.ExifInterface;
 import android.media.ThumbnailUtils;
 import android.os.Environment;
 import android.provider.MediaStore.Video.Thumbnails;
+import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 
@@ -19,11 +20,10 @@ import java.io.IOException;
 
 /**
  * Utility class for generating scaled Bitmaps and Thumbnails.
+ *
  * @author charmanesantiago
  */
 public class BabbleImageProcessorUtil {
-    private static final String TAG = BabbleImageProcessorUtil.class.getSimpleName();
-
     public static final String TYPE_VIDEO = "video";
     public static final String TYPE_IMAGE = "image";
     public static final String THUMBNAIL_DESTINATION_URL = Environment
@@ -64,14 +64,14 @@ public class BabbleImageProcessorUtil {
     public static void persistThumbnail(Bitmap bitmap, String id) {
         File fileDir = new File(THUMBNAIL_DESTINATION_URL);
         if (!fileDir.exists()) {
-            if(!fileDir.mkdirs()){
+            if (!fileDir.mkdirs()) {
                 return;
             }
         }
 
         File file = new File(THUMBNAIL_DESTINATION_URL + id + ".jpeg");
         try {
-            if(!file.createNewFile()){
+            if (!file.createNewFile()) {
                 return;
             }
         } catch (IOException e) {
@@ -95,7 +95,6 @@ public class BabbleImageProcessorUtil {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(CompressFormat.JPEG, 50, baos);
         byte[] b = baos.toByteArray();
-        Log.d(TAG, "Base 64 byte length " + b.length);
         return Base64.encodeToString(b, 0);
     }
 
@@ -113,68 +112,69 @@ public class BabbleImageProcessorUtil {
             }
         }
 
-        Log.d(TAG, "In Sample Size: " + inSampleSize);
         return inSampleSize;
     }
 
     public static Bitmap getResizeBitmap(String existingFileName) {
-        existingFileName = existingFileName.replace("file://", "");
+        if (!TextUtils.isEmpty(existingFileName)) {
+            existingFileName = existingFileName.replace("file://", "");
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(existingFileName, options);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(existingFileName, options);
 
-        int width = options.outWidth;
-        int height = options.outHeight;
+            int width = options.outWidth;
+            int height = options.outHeight;
 
-        Log.d(TAG, "Original bitmap width: " + width + " height: " + height);
-        if (width > THUMBNAIL_MAX_DIMENSION || height > THUMBNAIL_MAX_DIMENSION) {
-            if (width > height) {
-                double ratio = width * 1.0 / THUMBNAIL_MAX_DIMENSION;
+            if (width > THUMBNAIL_MAX_DIMENSION || height > THUMBNAIL_MAX_DIMENSION) {
+                if (width > height) {
+                    double ratio = width * 1.0 / THUMBNAIL_MAX_DIMENSION;
 
-                width = THUMBNAIL_MAX_DIMENSION;
-                height = (int)(height * 1.0 / ratio);
-            } else if (width < height) {
-                double ratio = height / THUMBNAIL_MAX_DIMENSION;
+                    width = THUMBNAIL_MAX_DIMENSION;
+                    height = (int) (height * 1.0 / ratio);
+                } else if (width < height) {
+                    double ratio = height / THUMBNAIL_MAX_DIMENSION;
 
-                width = (int)(width * 1.0 / ratio);
-                height = THUMBNAIL_MAX_DIMENSION;
-            } else {
-                width = THUMBNAIL_MAX_DIMENSION;
-                height = THUMBNAIL_MAX_DIMENSION;
+                    width = (int) (width * 1.0 / ratio);
+                    height = THUMBNAIL_MAX_DIMENSION;
+                } else {
+                    width = THUMBNAIL_MAX_DIMENSION;
+                    height = THUMBNAIL_MAX_DIMENSION;
+                }
             }
-        }
 
-        options.inSampleSize = calculateInSampleSize(options, width, height);
-        options.inJustDecodeBounds = false;
+            options.inSampleSize = calculateInSampleSize(options, width, height);
+            options.inJustDecodeBounds = false;
 
-        Bitmap bitmap = BitmapFactory.decodeFile(existingFileName, options);
-        Log.d(TAG, "Resized bitmap width: " + bitmap.getWidth() + " height: " + bitmap.getHeight());
-        if (bitmap == null) {
-            return null;
-        }
-
-        try {
-            ExifInterface exif = new ExifInterface(existingFileName);
-            Matrix matrix = new Matrix();
-
-            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1);
-
-            if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
-                matrix.postRotate(90);
-            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
-                matrix.postRotate(180);
-            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
-                matrix.postRotate(270);
+            Bitmap bitmap = BitmapFactory.decodeFile(existingFileName, options);
+            if (bitmap == null) {
+                return null;
             }
-            return Bitmap.createBitmap(bitmap, 0, 0,
-                    bitmap.getWidth(),
-                    bitmap.getHeight(), matrix, false);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } catch (OutOfMemoryError e) {
-            e.printStackTrace();
+
+            try {
+                ExifInterface exif = new ExifInterface(existingFileName);
+                Matrix matrix = new Matrix();
+
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1);
+
+                if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                    matrix.postRotate(90);
+                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                    matrix.postRotate(180);
+                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                    matrix.postRotate(270);
+                }
+                return Bitmap.createBitmap(bitmap, 0, 0,
+                        bitmap.getWidth(),
+                        bitmap.getHeight(), matrix, false);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
             return null;
         }
     }
